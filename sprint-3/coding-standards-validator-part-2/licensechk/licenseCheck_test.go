2af0d9adb89132195a5668993fad0f3304639086
JuanMoncada23 <54727154+JuanMoncada23@users.noreply.github.com>
2021-02-26 16:09:55 -0600

    All 4 test implemented

--- sprint-3/coding-standards-validator-part-2/licensechk/licenseCheck_test.go
+++ sprint-3/coding-standards-validator-part-2/licensechk/licenseCheck_test.go
@@ -7,9 +7,82 @@ import (
 
 const sep string = string(os.PathSeparator)
 
+var retCt int
+var retMsg, retIssue string
+var result bool
+
+var lc []LicenseChecker = []LicenseChecker{
+	{Path: ".." + sep + "test-01"},
+	{Path: ".." + sep + "test-02"},
+	{Path: ".." + sep + "test-03"},
+	{Path: ".." + sep + "test-04"},
+}
+
 func TestValidate01(t *testing.T) {
-	lc := LicenseChecker{Path: `..` + sep + `test-01`}
+	lc := lc[0]
+
+	result = lc.Validate()
+	retMsg = lc.GetMsg()
+	retIssue = lc.GetIssues()
+	retCt = lc.GetIssueCt()
+
 	if !lc.Validate() {
-		t.Error(`No License File`)
+		t.Error("No License File")
+	}
+	if result == false || retMsg == `` || retIssue != `` || retCt != 0 {
+		t.Error("Expected true, got", result, "Excepted 0 issues, got", retCt)
+	}
+
+}
+
+func TestValidate02(t *testing.T) {
+	lc := lc[1]
+
+	result = lc.Validate()
+	retMsg = lc.GetMsg()
+	retIssue = lc.GetIssues()
+	retCt = lc.GetIssueCt()
+
+	if !lc.Validate() {
+		t.Error("No License File")
+	}
+
+	if result == true || retMsg == `` || retIssue != `` || retCt != 0 {
+		t.Error("Expected false, got", result, "Excepted 0 issues, got", retCt)
 	}
 }
+func TestValidate03(t *testing.T) {
+	lc := lc[2]
+
+	result = lc.Validate()
+	retMsg = lc.GetMsg()
+	retIssue = lc.GetIssues()
+	retCt = lc.GetIssueCt()
+
+	if !lc.Validate() {
+		t.Error("No License File")
+	}
+
+	if result == true || retMsg == `` || retIssue != `` || retCt != 0 {
+		t.Error("Expected false, got", result, "Excepted 0 issues, got", retCt)
+	}
+}
+
+func TestValidate04( t *testing.T) {
+	lc := lc[3]
+
+	result = lc.Validate()
+	retMsg = lc.GetMsg()
+	retIssue = lc.GetIssues()
+	retCt = lc.GetIssueCt()
+
+	if !lc.Validate() {
+		t.Error("No License File")
+	}
+
+	if result == true || retMsg == `` || retIssue != `` || retCt != 0 {
+		t.Error("Expected false, got", result, "Excepted 0 issues, got", retCt)
+	}
+}
+
+
